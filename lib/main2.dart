3cea1422f5fb05bde48d3ae473713d36ad493d02
Ru <rickqaq@gmail.com>
2023-10-25 11:53:05 +0800

    將main的下方蔡單分開

---
+++ lib/main2.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'menu.dart';
+import 'sign_in.dart';
+import 'log_in.dart';
+/*
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(const MyApp());
+}
+ */
+bool isDarkMode = false;
+
+class main2 extends StatelessWidget {
+  const main2({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MyApp',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        colorSchemeSeed: Colors.brown,
+        brightness: Brightness.light,
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.brown,
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int index = 0;//選擇NavigationBar的項目
+  final screens = [
+    const log_in(),
+    const  menu(),
+    const sign_in(),
+  ];//傳送至頁面的陣列
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: index,
+        children: screens,
+      ),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          labelTextStyle: MaterialStateProperty.all(
+            const TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+          ),
+        ),
+        child: NavigationBar(
+          height: MediaQuery.of(context).size.height * 0.1,
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,//只顯示選中項目的文字
+          selectedIndex: index,
+          animationDuration: const Duration(milliseconds: 500),
+          onDestinationSelected: (index) => setState(() => this.index = index),//選定項目的結果
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.how_to_reg_rounded),
+              selectedIcon: Icon(Icons.how_to_reg_rounded),
+              label: '登入',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.restaurant_menu),
+              selectedIcon: Icon(Icons.restaurant_menu),
+              label: '更改菜單',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person_outlined),
+              selectedIcon: Icon(Icons.person_outlined),
+              label: '註冊',
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
